4663404d1261907e7346beff01fc84077dedf91d
Rudolf Vadovič <rudoman.svk@gmail.com>
2024-12-01 20:21:19 +0100

    Update src/test/java/sk/uniba/fmph/dcs/game_board/AllPlayersTakeRewardTest.java

--- src/test/java/sk/uniba/fmph/dcs/game_board/AllPlayersTakeRewardTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/AllPlayersTakeRewardTest.java
@@ -63,7 +63,9 @@ public class AllPlayersTakeRewardTest {
             }
         };
         RewardMenu menu = new RewardMenu(new ArrayList<>());
-        AllPlayersTakeReward a = new AllPlayersTakeReward(menu, null);
+        Throw t = new Throw();
+        AllPlayersTakeReward a = new AllPlayersTakeReward(menu, t);
+
         PlayerOrder playerOrder = new PlayerOrder(0,0);
         Player p = new Player(playerOrder, board);
         assertEquals(a.performEffect(p, null), ActionResult.ACTION_DONE_ALL_PLAYERS_TAKE_A_REWARD);
