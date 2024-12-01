381a6f0027e802a69d0627f01068f345b4116534
Rudolf Vadovič <rudoman.svk@gmail.com>
2024-12-01 20:19:40 +0100

    Update src/main/java/sk/uniba/fmph/dcs/game_board/GetChoice.java

--- src/main/java/sk/uniba/fmph/dcs/game_board/GetChoice.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/GetChoice.java
@@ -7,22 +7,17 @@ import java.util.ArrayList;
 public class GetChoice implements EvaluateCivilisationCardImmediateEffect {
     private int numberOfResources;
 
-    public GetChoice(final int numberOfResources) {
-        this.numberOfResources = numberOfResources;
-    }
+    public GetChoice() {}
 
     @Override
     public final ActionResult performEffect(final Player player, final Effect choice) {
         if (!choice.isResource()) {
             return ActionResult.FAILURE;
         }
-        if (numberOfResources <= 0) {
-            return ActionResult.FAILURE;
-        }
-        numberOfResources--;
         ArrayList<Effect> c = new ArrayList<>();
         c.add(choice);
         player.playerBoard().giveEffect(c);
         return ActionResult.ACTION_DONE;
     }
+
 }
