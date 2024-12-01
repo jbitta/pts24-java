e927fc5ccebb14c7b56c13857e7cf7ac8322efad
Rudolf Vadovič <rudoman.svk@gmail.com>
2024-12-01 20:19:29 +0100

    Update src/main/java/sk/uniba/fmph/dcs/game_board/GetCard.java
    Co-authored-by: Fremal31 <adam.fremal@gmail.com>

--- src/main/java/sk/uniba/fmph/dcs/game_board/GetCard.java
+++ src/main/java/sk/uniba/fmph/dcs/game_board/GetCard.java
@@ -1,24 +1,23 @@
 package sk.uniba.fmph.dcs.game_board;
 
-import sk.uniba.fmph.dcs.stone_age.ActionResult;
 import sk.uniba.fmph.dcs.stone_age.CivilisationCard;
+import sk.uniba.fmph.dcs.stone_age.ActionResult;
 import sk.uniba.fmph.dcs.stone_age.Effect;
 
-public class GetCard implements EvaluateCivilisationCardImmediateEffect {
-    private boolean used = false;
-    private final CivilisationCard card;
+import java.util.Collection;
+import java.util.Optional;
 
-    public GetCard(final CivilisationCard card) {
-        this.card = card;
+public class GetCard implements EvaluateCivilisationCardImmediateEffect {
+    private final CivilizationCardDeck deck;
+
+    public GetCard(final CivilizationCardDeck deck) {
+        this.deck = deck;
     }
 
-    @Override
-    public final ActionResult performEffect(final Player player, final Effect choice) {
-        if (used) {
-            return ActionResult.FAILURE;
-        }
-        used = true;
-        player.playerBoard().giveEndOfGameEffect((card.endOfGameEffect()));
+    public ActionResult performEffect(final Player player, final Effect choice) {
+        Optional<CivilisationCard> cardFromDeck = deck.getTop();
+        cardFromDeck.ifPresent(card -> player.playerBoard().giveEndOfGameEffect(card.endOfGameEffect()));
         return ActionResult.ACTION_DONE;
     }
 }
+
