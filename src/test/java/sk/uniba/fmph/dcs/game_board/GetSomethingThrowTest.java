f24febc34a69ecccc0370570ded408e8bbb635eb
Rudolf Vadovič <rudoman.svk@gmail.com>
2024-12-01 20:23:02 +0100

    Update src/test/java/sk/uniba/fmph/dcs/game_board/GetSomethingThrowTest.java

--- src/test/java/sk/uniba/fmph/dcs/game_board/GetSomethingThrowTest.java
+++ src/test/java/sk/uniba/fmph/dcs/game_board/GetSomethingThrowTest.java
@@ -10,8 +10,10 @@ public class GetSomethingThrowTest {
     @Test
     public void testCalculation() {
         Player p = new Player(null, null);
-        GetSomethingThrow getThrow = new GetSomethingThrow(Effect.WOOD, 2);
-        assertEquals(getThrow.performEffect(p, null), ActionResult.ACTION_DONE);
-        assertEquals(getThrow.performEffect(p, null), ActionResult.FAILURE);
+        CurrentThrow currentThrow = new CurrentThrow();
+        GetSomethingThrow getThrow = new GetSomethingThrow(currentThrow);
+        assertEquals(getThrow.performEffect(p, Effect.WOOD), ActionResult.ACTION_DONE);
+        assertEquals(getThrow.performEffect(p, Effect.FOOD), ActionResult.FAILURE);
     }
+
 }
