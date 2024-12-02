8ff281fbbfed2d7120fe6716d6c99d28caea8ade
samue-sudin <sudin1@uniba.sk>
2024-12-02 16:43:11 +0100

    Update src/test/java/sk/uniba/fmph/dcs/player_board/PlayerToolsTest.java
    Co-authored-by: jbitta <bittara3@uniba.sk>

--- src/test/java/sk/uniba/fmph/dcs/player_board/PlayerToolsTest.java
+++ src/test/java/sk/uniba/fmph/dcs/player_board/PlayerToolsTest.java
@@ -22,8 +22,10 @@ public class PlayerToolsTest {
 
         playerTools.addTool();
         state = new JSONObject(playerTools.state());
-        assertEquals("[1, 1][]", state.getString("tools"));
-        assertEquals("[false, false][]", state.getString("usedTools"));
+        assertEquals("[1, 1]", state.getString("tools"));
+        assertEquals("[]", state.getString("singleUseTools"));
+        assertEquals("[false, false]", state.getString("usedTools"));
+        assertEquals("[]", state.getString("usedSingleUseTools"));
 
         // Tretí nástroj sa pridá
         playerTools.addTool();
